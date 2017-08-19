23a21fa2e879cab25f490a49ee8184a5ed46f5c2
Windelle John Vega <windellevega@gmail.com>
2017-08-20 00:01:19 +0800

    -Added flash messages

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
-mongoose.connect('mongodb://localhost/nodedb');
+mongoose.connect('mongodb://localhost/nodedb', {
+		useMongoClient: true
+	}, 
+	function (ignore, connection) {
+		connection.onOpen();
+	}
+);
 let db = mongoose.connection;
 
 //Check connection
@@ -28,12 +37,46 @@ app.set('view engine', 'pug');
 
 //Body Parser Middleware
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extend: false }));
+app.use(bodyParser.urlencoded({ 
+	extended: true 
+}));
 // parse application/json
 app.use(bodyParser.json());
 
 //Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+	secret: 'ign4rus',
+	resave: true,
+	saveUninitialized: true
+  }));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value) {
+		var namespace = param.split('.')
+		, root = namespace.shift()
+		, formParam = root;
+
+		while(namespace.length) {
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		};
+	}
+}));
 
 //Home Route
 app.get('/', function(req, res) {
@@ -50,84 +93,9 @@ app.get('/', function(req, res) {
 	});
 });
 
-//Get Single Article
-app.get('/article/:id', function(req, res) {
-	Article.findById(req.params.id, function(err, article) {
-		res.render('article', {
-			article:article
-		});
-	});
-});
-
-//Add Route
-app.get('/articles/add', function(req, res) {
-	res.render('add_article', {
-		title:'Add Article'
-	});
-});
-
-//Add Submit POST route
-app.post('/articles/add', function(req, res) {
-	let article = new Article();
-	article.title = req.body.title;
-	article.author = req.body.author;
-	article.body = req.body.body;
-
-	article.save(function(err) {
-		if(err) {
-			console.log(err);
-			return;
-		}
-		else {
-			res.redirect('/');
-		}
-	});
-	//console.log(req.body.title);
-	//return;
-});
-
-//Load Edit Form
-app.get('/article/edit/:id', function(req, res) {
-	Article.findById(req.params.id, function(err, article) {
-		res.render('edit_article', {
-			title: 'Edit Article',
-			article:article
-		});
-	});
-});
-
-//Update Submit POST route
-app.post('/articles/edit/:id', function(req, res) {
-	let article = {};
-	article.title = req.body.title;
-	article.author = req.body.author;
-	article.body = req.body.body;
-
-	let query = {_id:req.params.id}
-
-	Article.update(query, article, function(err) {
-		if(err) {
-			console.log(err);
-			return;
-		}
-		else {
-			res.redirect('/');
-		}
-	});
-	//console.log(req.body.title);
-	//return;
-});
-
-app.delete('/article/:id', function(req, res) {
-	let query = {_id:req.params.id}
-
-	Article.remove(query, function(err) {
-		if(err) {
-			console.log(err);
-		}
-		res.send('Success');
-	});
-});
+//Route Files
+let articles = require('./routes/articles');
+app.use('/articles', articles);
 
 //Start Server
 app.listen('3000', function() {
